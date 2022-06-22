9620565117f0296d1aa7c02c9a5368bfc7b6e949
Kurt Dela Rosa <ak.delarosa@seiko-it.com.ph>
2022-06-23 00:04:53 +0800

    CU-2z01nz6 - FFT-7: Setup all pages -- page master and customer master module

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -1,29 +1,4 @@
 
-
-// const menu_list = () => {
-//     $.ajax({
-//         dataType: 'json',
-//         type: 'GET',
-//         url: '/helpers/menu_list',
-//     }).done( function(response, textStatus, xhr) {
-//         var url = window.location.href;
-//         var currentlUrl = url.replace("http://"+window.location.hostname,'');
-//         var menu = response.data;
-
-//         if (menu.length) {
-//             drawUserMenu(menu, currentlUrl);
-//             // if (menu.replace(" ", "") != "") {
-//             //     drawUserMenu(JSON.parse(menu), currentlUrl);
-//             // }
-//         }
-//     }).fail( function(xhr, textStatus, errorThrown) {
-
-//     }).always( function() {
-
-//     });
-// }
-
-
 const drawUserMenu = (menu,currentlUrl) => {
     const tmpForParentMenu = menu;
     const tmpForSubMenu = menu;
@@ -33,13 +8,9 @@ const drawUserMenu = (menu,currentlUrl) => {
             return _.omit(byCategory, 'parent_menu');
         });
     }));
-    //console.log(arrGroupedData)
-    var AccessType = 0;
-    var DeleteEnabled = 0;
+    
     var activeMainMenuLink = "";
     var activeSubMenuClass = "";
-    
-    var activeMainElement = '';
 
     $.each(arrGroupedData, function(i, groupedMainMenu) {
         if (i) {
@@ -52,15 +23,16 @@ const drawUserMenu = (menu,currentlUrl) => {
                     if (currentlUrl.includes(link.url)) {
                         activeSubMenuClass = "active";
                         if (mainActive == '') {
-                            mainActive = "menu-open";
+                            activeMainMenuLink = "active";
+                            mainActive = "menu-is-opening menu-open";
                         }
                     } else {
                         activeSubMenuClass = "";
                     }
 
                     if (link.has_sub > 0 && link.parent_name == "0") {
-                        menuBar += "<li class='nav-item " + mainActive + "'>" +
-                                        "<a href='"+link.url+"' class='nav-link'>" +
+                        menuBar += "<li class='nav-item " + mainActive + "' id='"+link.page_name+"_main_li'>" +
+                                        "<a href='"+link.url+"' class='nav-link "+activeMainMenuLink+"' id='"+link.page_name+"_main_a'>" +
                                             "<i class='nav-icon "+link.icon+"'></i>"+
                                             "<p>"+link.page_label+" <i class='right fas fa-angle-left'></i></p>"+
                                         "</a>"+
@@ -69,8 +41,8 @@ const drawUserMenu = (menu,currentlUrl) => {
 
 
                     } else if (link.has_sub == 0 && link.parent_name == "0") {
-                        menuBar += "<li class='nav-item " + mainActive + "'>" +
-                                        "<a href='"+link.url+"' class='nav-link "+activeSubMenuClass+"'>" +
+                        menuBar += "<li class='nav-item " + activeMainMenuLink + "'>" +
+                                        "<a href='"+link.url+"' class='nav-link "+mainActive+"'>" +
                                             "<i class='nav-icon "+link.icon+"'></i>"+
                                             "<p>"+link.page_label+"</p>"+
                                         "</a>"+
@@ -90,144 +62,18 @@ const drawUserMenu = (menu,currentlUrl) => {
             });
             $("#menuBar").append(menuBar);
             $(parent_id).append(subMenu);
-
-
-                
-            //}
-
-            // switch (i) {
-            //     case "System Maintenance":
-            //         menuBar += "<li class='has-sub " + activeMainMenuClass + "' id='SystemMaintenance'>\
-            //                         <a href='javascript:;'>\
-            //                             <b class='caret'></b>\
-            //                             <i class='fa fa-cogs'></i>\
-            //                             <span>System Maintenance</span>\
-            //                         </a>\
-            //                         <ul class='sub-menu'>";
-
-            //         $.each(groupedMainMenu, function(indx, link) {
-            //             if (currentlUrl.includes(link.url)) {
-            //                 activeSubMenuClass = "active";
-            //                 if (mainActive == '') {
-            //                     mainActive = "active";
-            //                 }
-            //             } else {
-            //                 activeSubMenuClass = "";
-            //             }
-
-            //             AccessType = (link.access == 0) ? 0 : 1;
-
-            //             if (AccessType) {
-            //                 menuBar += "<li class='" + activeSubMenuClass + "'>\
-            //                                     <a href='" + link.url + "'>" + link.title + "</a>\
-            //                                 </li>";
-            //             }
-            //         });
-
-            //         menuBar += "    </ul>";
-            //         menuBar += "</li>";
-
-            //         if (mainActive == 'active') {
-            //             activeMainElement = '#SystemMaintenance';
-            //         }
-            //         break;
-
-            //     case "Transaction":
-            //         menuBar += "<li class='has-sub " + activeMainMenuClass + "' id='Transaction'>\
-            //                         <a href='javascript:;'>\
-            //                             <b class='caret'></b>\
-            //                             <i class='fa fa-database'></i>\
-            //                             <span>Transaction</span>\
-            //                         </a>\
-            //                         <ul class='sub-menu'>";
-
-            //         $.each(groupedMainMenu, function(indx, link) {
-            //             if (currentlUrl.includes(link.url)) {
-            //                 activeSubMenuClass = "active";
-            //                 if (mainActive == '') {
-            //                     mainActive = "active";
-            //                 }
-            //             } else {
-            //                 activeSubMenuClass = "";
-            //             }
-
-            //             AccessType = (link.access == 0) ? 0 : 1;
-
-            //             if (AccessType) {
-            //                 menuBar += "<li class='" + activeSubMenuClass + "'>\
-            //                                     <a href='" + link.url + "'>" + link.title + "</a>\
-            //                                 </li>";
-            //             }
-            //         });
-
-            //         menuBar += "    </ul>";
-            //         menuBar += "</li>";
-
-            //         if (mainActive == 'active') {
-            //             activeMainElement = '#Transaction';
-            //         }
-            //         break;
-
-            //     case "Reports":
-            //         menuBar += "<li class='nav-item " + activeMainMenuClass + "' id='Reports'>" +
-            //                         "<a href='#' class='nav-link active'>" +
-            //                             "<i class='nav-icon fas fa-tachometer-alt'></i>"+
-            //                             "<p>Reports <i class='right fas fa-angle-left'></i></p>"+
-            //                         "</a>"+
-            //                         "<ul class='nav nav-treeview'>";
-
-            //         $.each(groupedMainMenu, function(indx, link) {
-            //             if (currentlUrl.includes(link.url)) {
-            //                 activeSubMenuClass = "active";
-            //                 if (mainActive == '') {
-            //                     mainActive = "active";
-            //                 }
-            //             } else {
-            //                 activeSubMenuClass = "";
-            //             }
-
-            //             AccessType = (link.authorize == 0) ? 0 : 1;
-
-            //             if (AccessType) {
-            //                 menuBar += "<li class='nav-link " + activeSubMenuClass + "'>"+
-            //                                     "<a href='" + link.url + "'>"+
-            //                                         "<i class='" + link.icon + " nav-icon'></i>"+
-            //                                         "<p>" + link.page_label + "</p>"+
-            //                                     "</a>"+
-            //                                 "</li>";
-            //             }
-            //         });
-
-            //         menuBar += "    </ul>";
-            //         menuBar += "</li>";
-
-            //         if (mainActive == 'active') {
-            //             activeMainElement = '#Reports';
-            //             $(activeMainElement).addClass("menu-open");
-            //             $(activeMainElement + '.nav-link').addClass(mainActive);
-            //         }
-            //         break;
-
-            //     default:
-            //         break;
-            // }
+            $(parent_id+"_main_li").addClass(mainActive);
+            $(parent_id+"_main_a").addClass(mainActive);
         }
     });
-    
-    $(".has-sub").removeClass("active");
-    
 }
 
-$( function() {
-    // menu_list();
-    var menu = $("#hdnMenu").val();
-    var currentlUrl = $("#hdnMenu").data("currurl");
+var menu = $("#hdnMenu").val();
+var currentlUrl = $("#hdnMenu").data("currurl");
 
-    if ($("#hdnMenu").length) {
-        if (menu.replace(" ", "") != "") {
-            drawUserMenu(JSON.parse(menu),currentlUrl);
-        }
+if ($("#hdnMenu").length) {
+    if (menu.replace(" ", "") != "") {
+        drawUserMenu(JSON.parse(menu),currentlUrl);
     }
-    
-});
+}
 
