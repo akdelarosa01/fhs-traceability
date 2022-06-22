75f695cbb639a8987b895ed5266b33f1a96e8113
Kurt Dela Rosa <ak.delarosa@seiko-it.com.ph>
2022-06-22 14:23:42 +0800

    Added Login Validation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\PalletPageAccess;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -43,5 +47,55 @@ class LoginController extends Controller
     public function username()
     {
         return 'username';
+    }
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return Exception
+     */
+    public function validateLogin(Request $req)
+    {
+        $user_count = User::where('username', $req->username)->count();
+
+        if($user_count > 0) {
+            $user = User::where('username', $req->username)->first();
+
+            if ($user_count > 1) {
+                $user = User::where('username', $req->username)->orderBy('id','desc')->first();
+            }
+
+            if ($user->password !== bcrypt($req->password)) {
+                throw ValidationException::withMessages(['password' => __("Password is incorrect.")]);
+            } elseif ($user->is_deleted == 1) {
+                throw ValidationException::withMessages([$this->username() => __("User account was deactivated, Please contact your administrator.")]);
+            } else {
+
+                return redirect()->intended('home');
+            }
+        } else {
+            throw ValidationException::withMessages([$this->username() => __("Username doesn't exists. Please contact your administrator.")]);
+        }
+
+        return $req->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+        ]);
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $req, $user)
+    {
+        $page = new PalletPageAccess();
+        $user_access = $page->menu_list($user->id)->toJson();
+        $req->session()->put('user_accesses', $user_access);
     }
 }
