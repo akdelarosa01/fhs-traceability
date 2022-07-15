4ee2543e2112fcffabc659d3d276c37823d28c6a
Kurt Dela Rosa <ak.delarosa@seiko-it.com.ph>
2022-07-15 18:31:18 +0800

    CU-326w1ez - Box-Pallet App - Print /Re-print button

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PageSeeder::class,
             PageAccessSeeder::class,
+            ModelSeeder::class
         ]);
     }
 }
