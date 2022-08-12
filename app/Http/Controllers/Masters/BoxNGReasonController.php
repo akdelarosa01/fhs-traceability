6ea21398b60a3be8739a3305e919fbeb178f3957
Kurt Dela Rosa <ak.delarosa01@gmail.com>
2022-08-12 14:32:34 +0800

    CU-39brjc1 - Box Not Good Reason Master - Delete function

--- app/Http/Controllers/Masters/BoxNGReasonController.php
+++ app/Http/Controllers/Masters/BoxNGReasonController.php
@@ -141,4 +141,61 @@ class BoxNGReasonController extends Controller
 
         return response()->json($data);
     }
+
+    public function delete_reason(Request $req)
+    {
+        $data = [
+			'msg' => 'Deleting Box N.G. Reason has failed.',
+            'data' => [],
+			'success' => true,
+            'msgType' => 'warning',
+            'msgTitle' => 'Failed!'
+        ];
+
+        try {
+            if (is_array($req->ids)) {
+                $update = PalletBoxNgReason::whereIn('id',$req->ids)
+                            ->update([
+                                'is_deleted' => 1,
+                                'update_user' => Auth::user()->id,
+                                'updated_at' => date('Y-m-d H:i:s')
+                            ]);
+                if ($update) {
+                    $data = [
+                        'msg' => "Box N.G. Reason was successfully deleted.",
+                        'data' => [],
+                        'success' => true,
+                        'msgType' => 'success',
+                        'msgTitle' => 'Success!'
+                    ];
+                }
+                
+            } else {
+                $update = PalletBoxNgReason::where('id',$req->ids)
+                            ->update([
+                                'is_deleted' => 1,
+                                'update_user' => Auth::user()->id,
+                                'updated_at' => date('Y-m-d H:i:s')
+                            ]);
+                if ($update) {
+                    $data = [
+                        'msg' => "Box N.G. Reason was successfully deleted.",
+                        'data' => [],
+                        'success' => true,
+                        'msgType' => 'success',
+                        'msgTitle' => 'Success!'
+                    ];
+                }
+            }
+        } catch (\Throwable $th) {
+            $data = [
+                'msg' => $th->getMessage(),
+                'data' => [],
+                'success' => false,
+                'msgType' => 'error',
+                'msgTitle' => 'Error!'
+            ];
+        }
+        return response()->json($data);
+    }
 }
