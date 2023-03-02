f17b97d0277a50766619b87c6ef274f6abc41307
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-03-02 09:41:37 +0800

    color change in shipment status

--- resources/js/transactions/warehouse.js
+++ resources/js/transactions/warehouse.js
@@ -233,10 +233,11 @@
                             case 'ONLOAD':
                                 $(dataRow[0].cells[8]).css('background-color', 'lightblue');
                                 $(dataRow[0].cells[8]).css('color', '#000000');
-                            break;
+                                break;
                             default:
-                                $(dataRow[0].cells[8]).css('background-color', 'white');
+                                $(dataRow[0].cells[8]).css('background-color', '#FFC000');
                                 $(dataRow[0].cells[8]).css('color', '#000000');
+                                break;
                         }
                     },
                     initComplete: function() {
